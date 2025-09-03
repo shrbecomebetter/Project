ae9cfe835b5264b8a9a1e37210740fe7395fe39c
Your Name <you@example.com>
2025-09-03 13:18:41 +0800

    json print

--- weather.py
+++ weather.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+import requests
+city=input("Enter the city name: ")
+url=f"https://v2.xxapi.cn/api/weather?city={city}"
+response=requests.get(url)
+data=response.json()
+print(f"The weather in {data['data']['city']}is{data['data']['data'][0]['weather']}")
